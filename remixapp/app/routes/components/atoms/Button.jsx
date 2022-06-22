a73dec0c949b2d868aa320594bc2915e10bddfc0
user <user@userui-MacBookPro.local>
2022-06-22 11:05:24 +0900

    remix error (className did not match)

--- remixapp/app/routes/components/atoms/Button.jsx
+++ remixapp/app/routes/components/atoms/Button.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import styled from 'styled-components';
 
-function Button({children , color , background}) {
-    const StyledButton = styled.button`
-        font-size: 1rem; 
-        font-weight: 500;
-        color: ${prop => prop.color || "#000"};
-        background: ${prop => prop.background || "aqua"};
-        border: none;
-        border-radius: 5px;
-        padding: 0.3rem 1rem; 
-        min-width: 50px;
-        cursor: pointer;
-        & + button {
-            margin: 0 0 0 1rem;
-        }
-    `;
+const StyledButton = styled.button`
+    font-size: 1rem; 
+    font-weight: 500;
+    color: ${prop => prop.color || "#000"};
+    background: ${prop => prop.background || "aqua"};
+    border: none;
+    border-radius: 5px;
+    padding: 0.3rem 1rem; 
+    min-width: 50px;
+    cursor: pointer;
+    & + button {
+        margin: 0 0 0 1rem;
+    }
+`;
 
+function Button({children , color , background}) {
   return (
-    <>
-        <StyledButton color={color} background={background}>{children}</StyledButton>
-    </>
+    <StyledButton color={color} background={background}>{children}</StyledButton>
   )
 }
 
--- remixapp/app/routes/index.jsx
+++ remixapp/app/routes/index.jsx
@@ -2,11 +2,11 @@ import Button from "./components/atoms/Button";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <>
         <Button>짧은</Button>
         <Button color="#000" background="#ffff99">긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글</Button>
         <Button color="#000" background="#ff9999">Hello</Button>
         <Button color="#fff" background="#9999ff">hi</Button>
-    </div>
+    </>
   );
 }
