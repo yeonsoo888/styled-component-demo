41fc927d85d9c52c431c001f34731c47fd69f6f6
user <user@userui-MacBookPro.local>
2022-06-22 13:36:57 +0900

    수정

--- cra/src/App.js
+++ cra/src/App.js
@@ -8,8 +8,12 @@ function App() {
     <div className="App" style={{display: "flex", flexWrap:"wrap", gap: "0.5rem", justifyContent: "center",}}>
         <Button color="primary" outline={true} size="sm">짧은</Button>
         <Button color="secondary" size="large">긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글긴글</Button>
-        <Button color="warning" block={true} active={true}>Hello</Button>
-        <Button color="success" block={true} outline={true}>hi</Button>
+        <Button color="success">Hello</Button>
+        <Button color="danger" >hi</Button>
+        <Button color="warning" >hi</Button>
+        <Button color="info" active={true} >hi</Button>
+        <Button color="light" >hi</Button>
+        <Button color="dark" block={true} outline={true}>hi</Button>
     </div>
   );
 }
